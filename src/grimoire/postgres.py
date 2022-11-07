b2ac979f3b3424e8756548d2d20514f0a3b5cc11
Eleanor Young <developer@nausicaea.net>
2022-11-07 16:12:05 +0100

    Added a description to the postgres connection spell

--- src/grimoire/postgres.py
+++ src/grimoire/postgres.py
@@ -1,13 +1,63 @@
-from contextlib import asynccontextmanager
-from collections.abc import AsyncGenerator
-
 try:
+    from asyncio import AbstractEventLoop
+    from contextlib import asynccontextmanager
+    from collections.abc import (
+        AsyncGenerator,
+        Sequence,
+    )
+    from typing import (
+        Literal,
+        Type,
+    )
+
     import asyncpg
+    import asyncpg.protocol
+    import asyncpg.connect_utils
 
 
     @asynccontextmanager
-    async def connect(dsn: str, password: str) -> AsyncGenerator[asyncpg.Connection, None]:
-        con: asyncpg.Connection = await asyncpg.connect(dsn, password=password)
+    async def connect(dsn: str | list[str] | None = None, *,
+                      host: str | list[str] | None = None,
+                      port: int | str | list[int] | None = None,
+                      user: str | None = None,
+                      password: str | None = None,
+                      passfile: str | None = None,
+                      database: str | None = None,
+                      loop: AbstractEventLoop | None = None,
+                      timeout: int | float = 60,
+                      statement_cache_size: int = 100,
+                      max_cached_statement_lifetime: int | float = 300,
+                      max_cacheable_statement_size: int = 1024 * 15,
+                      command_timeout: int | float | None = None,
+                      ssl: Literal[True] | str | asyncpg.connect_utils.SSLMode | None = None,
+                      direct_tls: bool = False,
+                      connection_class: Type[asyncpg.Connection] = asyncpg.Connection,
+                      record_class: Type[asyncpg.protocol.Record] = asyncpg.protocol.Record,
+                      server_settings: dict[str, str] | None = None,
+                      ) -> AsyncGenerator[asyncpg.Connection, None]:
+        """
+        Open a connection to Postgres by way of context manager. This ensures that any remaining traces of a connection
+        are closed up even if something bad has happened in-between.
+        """
+        con: asyncpg.Connection = await asyncpg.connect(dsn,
+                                                        host=host,
+                                                        port=port,
+                                                        user=user,
+                                                        password=password,
+                                                        passfile=passfile,
+                                                        database=database,
+                                                        loop=loop,
+                                                        timeout=timeout,
+                                                        statement_cache_size=statement_cache_size,
+                                                        max_cached_statement_lifetime=max_cached_statement_lifetime,
+                                                        max_cacheable_statement_size=max_cacheable_statement_size,
+                                                        command_timeout=command_timeout,
+                                                        ssl=ssl,
+                                                        direct_tls=direct_tls,
+                                                        connection_class=connection_class,
+                                                        record_class=record_class,
+                                                        server_settings=server_settings,
+                                                        )
         try:
             yield con
         finally:
@@ -16,4 +66,3 @@ try:
 
 except ImportError as ex:
     asyncpg = ex
-
