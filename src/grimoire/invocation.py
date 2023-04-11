822b78aa9f91f3a3de2db3e5dc706583b874dccf
Eleanor Young <developer@nausicaea.net>
2023-04-11 13:38:08 +0200

    Added type information to start()

--- src/grimoire/invocation.py
+++ src/grimoire/invocation.py
@@ -1,10 +1,10 @@
 import asyncio
 import functools
 import sys
-from typing import Callable, Coroutine
+from typing import Any, Callable, Coroutine
 
 
-def start(coro: Callable[[], Coroutine]) -> Callable[[], None]:
+def start(coro: Callable[[], Coroutine[Any, Any, Any]]) -> Callable[[], None]:
     """
     Wraps asynchronous invocation functions such that you may call them from a synchronous context.
     Use this to easily create an asynchronous main function, for example.
