8ef7310a2bda7bb1c938ad0e0ccf988a84440c9a
Eleanor Young <developer@nausicaea.net>
2023-04-12 10:48:23 +0200

    Added inject

--- src/grimoire/chronicle/handler_config.py
+++ src/grimoire/chronicle/handler_config.py
@@ -1,135 +1,140 @@
-from dataclasses import dataclass, field
-from pathlib import Path
+try:
+    from dataclasses import dataclass, field
+    from pathlib import Path
 
-from mashumaro import DataClassDictMixin, field_options
+    from mashumaro import DataClassDictMixin, field_options
 
-
-@dataclass
-class HandlerConfig(DataClassDictMixin):
-    """
-    Holds configuration for :class:`logging.Handler`
-    """
-
-    class_: str = field(metadata=field_options(alias="class"))
-    level: int | None = field(default=None)
-    formatter: str | None = field(default=None)
-    stream: str | None = field(default=None)
-    filename: str | None = field(default=None)
-    delay: bool | None = field(default=None)
-    address: str | None = field(default=None)
-    rich_tracebacks: bool | None = field(default=None)
-    maxBytes: int = field(default=0)
-    backupCount: int = field(default=0)
-
-    @classmethod
-    def stderr_stream_handler(cls, level: int, formatter: str) -> "HandlerConfig":
+    @dataclass
+    class HandlerConfig(DataClassDictMixin):
         """
-        Create a configuration entry for :class:`logging.StreamHandler`
-
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :return:
+        Holds configuration for :class:`logging.Handler`
         """
-        return HandlerConfig(
-            class_="logging.StreamHandler",
-            formatter=formatter,
-            level=level,
-            stream="ext://sys.stderr",
-        )
 
-    @classmethod
-    def rich_handler(cls, level: int, rich_tracebacks: bool = True) -> "HandlerConfig":
-        """
-        Create a configuration entry for :class:`rich.logging.RichHandler`
+        class_: str = field(metadata=field_options(alias="class"))
+        level: int | None = field(default=None)
+        formatter: str | None = field(default=None)
+        stream: str | None = field(default=None)
+        filename: str | None = field(default=None)
+        delay: bool | None = field(default=None)
+        address: str | None = field(default=None)
+        rich_tracebacks: bool | None = field(default=None)
+        maxBytes: int = field(default=0)
+        backupCount: int = field(default=0)
 
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :return:
-        """
-        return HandlerConfig(
-            class_="rich.logging.RichHandler",
-            level=level,
-            rich_tracebacks=rich_tracebacks,
-        )
+        @classmethod
+        def stderr_stream_handler(cls, level: int, formatter: str) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`logging.StreamHandler`
 
-    @classmethod
-    def file_handler(
-        cls, file_name: Path, level: int, formatter: str
-    ) -> "HandlerConfig":
-        """
-        Create a configuration entry for :class:`logging.FileHandler`
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :return:
+            """
+            return HandlerConfig(
+                class_="logging.StreamHandler",
+                formatter=formatter,
+                level=level,
+                stream="ext://sys.stderr",
+            )
 
-        :param file_name: the name of the log output file
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :return:
-        """
-        return HandlerConfig(
-            class_="logging.FileHandler",
-            formatter=formatter,
-            level=level,
-            filename=str(file_name),
-            delay=True,
-        )
+        @classmethod
+        def rich_handler(
+            cls, level: int, rich_tracebacks: bool = True
+        ) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`rich.logging.RichHandler`
 
-    @classmethod
-    def rotating_file_handler(
-        cls,
-        file_name: Path,
-        level: int,
-        formatter: str,
-        max_bytes: int = 0,
-        backup_count: int = 0,
-    ) -> "HandlerConfig":
-        """
-        Create a configuration entry for :class:`logging.handlers.RotatingFileHandler`
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :return:
+            """
+            return HandlerConfig(
+                class_="rich.logging.RichHandler",
+                level=level,
+                rich_tracebacks=rich_tracebacks,
+            )
 
-        :param file_name: the name of the log output file
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :param max_bytes: the maximum size of a log file
-        :param backup_count: the number of log file backups to keep
-        :return:
-        """
-        return HandlerConfig(
-            class_="logging.handlers.RotatingFileHandler",
-            formatter=formatter,
-            level=level,
-            filename=str(file_name),
-            delay=True,
-            maxBytes=max_bytes,
-            backupCount=backup_count,
-        )
+        @classmethod
+        def file_handler(
+            cls, file_name: Path, level: int, formatter: str
+        ) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`logging.FileHandler`
 
-    @classmethod
-    def journald_handler(cls, level: int, formatter: str) -> "HandlerConfig":
-        """
-        Create a configuration entry for :class:`systemd.journal.JournalHandler` from the package systemd-python
+            :param file_name: the name of the log output file
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :return:
+            """
+            return HandlerConfig(
+                class_="logging.FileHandler",
+                formatter=formatter,
+                level=level,
+                filename=str(file_name),
+                delay=True,
+            )
 
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :return:
-        """
-        return HandlerConfig(
-            class_="systemd.journal.JournalHandler",
-            formatter=formatter,
-            level=level,
-        )
+        @classmethod
+        def rotating_file_handler(
+            cls,
+            file_name: Path,
+            level: int,
+            formatter: str,
+            max_bytes: int = 0,
+            backup_count: int = 0,
+        ) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`logging.handlers.RotatingFileHandler`
 
-    @classmethod
-    def syslog_handler(
-        cls, level: int, formatter: str, address="/dev/log"
-    ) -> "HandlerConfig":
-        """
-        Create a configuration entry for :class:`logging.handlers.SysLogHandler`
+            :param file_name: the name of the log output file
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :param max_bytes: the maximum size of a log file
+            :param backup_count: the number of log file backups to keep
+            :return:
+            """
+            return HandlerConfig(
+                class_="logging.handlers.RotatingFileHandler",
+                formatter=formatter,
+                level=level,
+                filename=str(file_name),
+                delay=True,
+                maxBytes=max_bytes,
+                backupCount=backup_count,
+            )
 
-        :param level: a logging level
-        :param formatter: the name of a formatter
-        :return:
-        """
-        return HandlerConfig(
-            class_="logging.handlers.SysLogHandler",
-            formatter=formatter,
-            level=level,
-            address=address,
-        )
+        @classmethod
+        def journald_handler(cls, level: int, formatter: str) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`systemd.journal.JournalHandler` from the package systemd-python
+
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :return:
+            """
+            return HandlerConfig(
+                class_="systemd.journal.JournalHandler",
+                formatter=formatter,
+                level=level,
+            )
+
+        @classmethod
+        def syslog_handler(
+            cls, level: int, formatter: str, address: str = "/dev/log"
+        ) -> "HandlerConfig":
+            """
+            Create a configuration entry for :class:`logging.handlers.SysLogHandler`
+
+            :param level: a logging level
+            :param formatter: the name of a formatter
+            :return:
+            """
+            return HandlerConfig(
+                class_="logging.handlers.SysLogHandler",
+                formatter=formatter,
+                level=level,
+                address=address,
+            )
+
+except ImportError as ex:
+    mashumaro = ex
--- src/grimoire/onepassword.py
+++ src/grimoire/onepassword.py
@@ -1,4 +1,5 @@
 import asyncio.subprocess
+from os import PathLike
 
 
 async def recall(item: str, field: str, vault: str | None = None) -> str:
@@ -20,6 +21,40 @@ async def recall(item: str, field: str, vault: str | None = None) -> str:
 
     if vault is not None:
         args.insert(3, f"--vault={vault}")
+
+    p = await asyncio.subprocess.create_subprocess_exec(
+        *args,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
+    stdout, stderr = await p.communicate()
+
+    if p.returncode != 0:
+        raise RuntimeError(stderr.decode("utf-8"))
+
+    return stdout.decode("utf-8").strip()
+
+
+async def inject(template: PathLike[str]) -> str:
+    """
+    Inject abyssal knowledge into an object of your choosing. Note however,
+    that this change is ephemeral.
+
+    >>> import asyncio
+    >>> import tempfile
+    >>> from grimoire.onepassword import inject
+    >>> with tempfile.NamedTemporaryFile(mode="wt") as tf:
+    ...   _ = tf.write("credential: {{ op://Temporary Items/Retrieval Marker/password }}")
+    ...   _ = tf.seek(0)
+    ...   asyncio.run(inject(tf.name))
+    'credential: q47h9HUmTdC2PBycx24znF2PHgpyYdJT'
+    """
+    args = [
+        "op",
+        "inject",
+        f"--in-file={template}",
+    ]
 
     p = await asyncio.subprocess.create_subprocess_exec(
         *args,
