c935628898f0262651def3e8a1af3f544c92d101
Eleanor Young <developer@nausicaea.net>
2024-06-13 21:55:18 +0200

    Never used this propertly, so let's restart :D

--- tests/test_chronicle.py
+++
@@ -1,6 +0,0 @@
-from grimoire.chronicle.formatter_config import (_DEFAULT_FORMAT,
-                                                 FormatterConfig)
-
-
-def test_formatter_config_has_format_field_with_default_value() -> None:
-    assert FormatterConfig().format == _DEFAULT_FORMAT
