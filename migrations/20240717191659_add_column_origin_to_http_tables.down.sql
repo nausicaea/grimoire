ac951dc7cae54edafaae361d8f889bd811c7dbd2
Eleanor Young <developer@nausicaea.net>
2024-07-18 07:53:15 +0200

    Fixed the most recent migration

--- migrations/20240717191659_add_column_origin_to_http_tables.down.sql
+++ migrations/20240717191659_add_column_origin_to_http_tables.down.sql
@@ -1,6 +1,3 @@
 -- Add down migration script here
 ALTER TABLE "http-recon" DROP COLUMN domain;
-UPDATE "http-recon" SET domain = (SELECT regexp_replace(fqdn, '^(?:[a-zA-Z0-9-]+\.)*((?:[a-zA-Z0-9-]+\.)(?:[a-zA-Z0-9-]+))$', '\1') AS domain FROM "http-recon") WHERE domain IS NULL;
-
 ALTER TABLE "https-recon" DROP COLUMN domain;
-UPDATE "https-recon" SET domain = (SELECT regexp_replace(fqdn, '^(?:[a-zA-Z0-9-]+\.)*((?:[a-zA-Z0-9-]+\.)(?:[a-zA-Z0-9-]+))$', '\1') AS domain FROM "https-recon") WHERE domain IS NULL;
--- migrations/20240717191659_add_column_origin_to_http_tables.up.sql
+++ migrations/20240717191659_add_column_origin_to_http_tables.up.sql
@@ -1,3 +1,5 @@
 -- Add up migration script here
 ALTER TABLE "http-recon" ADD COLUMN domain VARCHAR(256);
 ALTER TABLE "https-recon" ADD COLUMN domain VARCHAR(256);
+UPDATE "http-recon" SET domain = (SELECT regexp_replace(fqdn, '^(?:[a-zA-Z0-9-]+\.)*((?:[a-zA-Z0-9-]+\.)(?:[a-zA-Z0-9-]+))$', '\1') AS domain FROM "http-recon") WHERE domain IS NULL;
+UPDATE "https-recon" SET domain = (SELECT regexp_replace(fqdn, '^(?:[a-zA-Z0-9-]+\.)*((?:[a-zA-Z0-9-]+\.)(?:[a-zA-Z0-9-]+))$', '\1') AS domain FROM "https-recon") WHERE domain IS NULL;
